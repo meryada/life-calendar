ca9c0d20b20d6fa68e75d2837f8d5e3bd9b9899a
María Mata <matah.maria@gmail.com>
2019-11-01 18:41:43 +0100

    controled empty array at smileyList

--- src/components/SmileyList.js
+++ src/components/SmileyList.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
 class CalendarSmiley extends React.Component {
-    
+
     render() {
-        const {smileys} = this.props
-        console.log(smileys)
-        return(
-            <div>
-            <h2>calendario</h2>
-            <ul>
-                {smileys.map((item, index)=> {
-                    return (
-                        <li key={index}>
-                        <p>{item.date}</p>    
-                        <p>{item.status === 'feliz'? <i className="far fa-smile-beam"></i>:<i className="far fa-frown-open"></i>}</p>
-                        <p>{item.message}</p>
-                        </li>
-                    )
-                })}
-            </ul>
-            </div>
-        )
+        const { smileys } = this.props
+        console.log(smileys[1])
+        if (smileys[1]) {
+            return (
+                <div>
+                    <h2>calendario</h2>
+                    <ul>
+                        {smileys.map((item, index) => {
+                            return (
+                                <li key={index}>
+                                    <p>{item.date}</p>
+                                    <p>{item.status === 'feliz' ? <i className="far fa-smile-beam"></i> : <i className="far fa-frown-open"></i>}</p>
+                                    <p>{item.message}</p>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            )
+        } else {
+            return (
+                <p>no hay ningún estado</p>
+            )
+        }
     }
 }
 
